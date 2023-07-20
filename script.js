98e33696d2aaf08ae1649e9be38a2429d0f01981
Uday <udayomsrivastava111@gmail.com>
2023-07-21 00:31:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add your JavaScript code here
+// Initialize Tilt.js for committee images
+$('.committee-image').tilt({
+    maxTilt: 20,
+    perspective: 1000,
+    scale: 1.05,
+    speed: 300,
+});
+
+// You can add additional JavaScript code here for any other interactive features or bonus functionalities.
