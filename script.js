05bfcf207ec681bb1b12b8fadd9e5aa5d0df953f
Uday <udayomsrivastava111@gmail.com>
2023-07-22 18:28:29 +0530

    changing the thought-images content to come from the javascript file

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ VanillaTilt.init(document.querySelectorAll(".discover-committees-img, .discover-
     glare: true,
     "max-glare": 0.2,
 });
+
+
+const thoughtImages = document.querySelector(".thought-images");
+
+for (let i = 0; i < 64; i++) 
+{
+    let num = Math.floor(Math.random() * 360);
+    let content = "./images/" + (i % 2 == 0 ? "bulb.svg" : "hand.svg");
+
+    thoughtImages.innerHTML += `<div> <img src="${ content }" style="transform: rotate(${ num }deg);"> </div>`;
+}
